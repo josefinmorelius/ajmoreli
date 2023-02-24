b5561baedd8701aff9c0e42b4ff17631f4c001b3
Josefin Morelius <josmor@kth.se>
2023-02-24 11:33:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,14 +6,22 @@ const createscene = () => {
 
     const range = 50;
 
-    const camera_h_w = 2;
+    var camera_h_w = window.innerWidth / window.innerHeight;
 
     const near = 0.1;
 
-    const far = 5;
+    const far = 100;
 
     const camera = new THREE.PerspectiveCamera(range, camera_h_w, near, far);
 
+    camera.position.set( 1, 1.5, 2 );
+    camera.lookAt(0,0,0);
+
+    cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
+    cameraControls.target.set( 0, 0, 0 );
+
+    window.addEventListener('resize', updateAspectRatio );
+    
     const scene = new THREE.Scene();
 
     const depth = 1;
@@ -24,8 +32,11 @@ const createscene = () => {
 
     const material = new THREE.MeshBasicMaterial({color: C5C2CC})
 
-    const box = new THREE.Mesh(geometry, material);
+    const box = new THREE.Mesh(figure, material);
     scene.add(box);
+
+    renderer.render(scene, camera)
+    document.body.appendChild(renderer.domElement);
 
 };
 
