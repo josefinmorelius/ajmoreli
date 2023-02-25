86c7afa69a19cc60d22fbc0b86a890fa441ea012
Josefin Morelius <josmor@kth.se>
2023-02-25 12:55:14 +0100

    another try with 3d scene

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
-const createscene = () => {
+function createscene () 
+{
 
     const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor( new THREE.Color(0xFFFFFF) );
+    document.body.appendChild(renderer.domElement);
 
     const range = 50;
 
@@ -17,11 +20,11 @@ const createscene = () => {
     camera.position.set( 1, 1.5, 2 );
     camera.lookAt(0,0,0);
 
-    cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
+    var cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
     cameraControls.target.set( 0, 0, 0 );
 
     window.addEventListener('resize', updateAspectRatio );
-    
+
     const scene = new THREE.Scene();
 
     const depth = 1;
